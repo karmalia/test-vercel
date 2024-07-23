ab654611702da4c31d5ed8758887441fc5168d44
ismail <ismailsevgi95@gmail.com>
2024-07-23 12:24:45 +0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,107 @@
 import Image from "next/image";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Result, Root, Info, PersonRoot } from "@/types";
+import Filter from "@/components/Filter";
 
-export default function Home() {
+// All Dead Alive
+// All location
+
+const fetchChars = async (charList: string[]) => {
+  const data = await Promise.all(
+    charList.map(async (char) => {
+      const response = await fetch(char);
+      const data = await response.json();
+      return data;
+    })
+  );
+
+  return data;
+};
+
+// Search params to go
+const getData = async (location?: string) => {
+  let baseUrl = "https://rickandmortyapi.com/api/location";
+
+  const response = await fetch(baseUrl);
+  const data = await response.json();
+  const locations = data.results.map((res) => res.name);
+  let chars = [];
+  if (!location) {
+    console.log("2");
+    chars = await data.results[0].residents;
+    const characters = await fetchChars(chars);
+
+    return { locations, characters };
+  } else {
+    console.log("1");
+    chars = await data.results.find((res: Location) => location === res.name)
+      .residents;
+    const characters = await fetchChars(chars);
+    return { locations, characters };
+  }
+};
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { status: string; location: string };
+}) {
+  const { locations, characters } = await getData(searchParams.location);
+  console.log("chars", characters);
+  if (!searchParams.location) {
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className="flex min-h-screen flex-col gap-4 p-24 bg-slate-900">
+      <Filter locations={locations} searchParams={searchParams} />
+      <div className="grid grid-cols-4 gap-4">
+        {characters
+          .filter((char) => {
+            if (searchParams.status) {
+              return char.status === searchParams.status;
+            } else {
+              return char;
+            }
+          })
+          .map((character: PersonRoot) => (
+            <Card
+              key={character.id}
+              className="bg-gray-600 pt-4 grid grid-cols-3"
+            >
+              <CardContent className="col-span-1 x">
+                <Image
+                  src={character.image}
+                  alt={character.name}
+                  width={400}
+                  height={400}
+                  className="w-full h-full object-cover "
+                />
+              </CardContent>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+              <div>
+                <CardTitle className="text-white">{character.name}</CardTitle>
+                <div className="flex text-white flex-col items-start gap-2">
+                  <div className="flex items-center gap-2">
+                    <div
+                      className={`${
+                        character.status === "Alive"
+                          ? "bg-green-400"
+                          : "bg-red-600"
+                      } w-2 h-2 rounded-xl`}
+                    />
+                    {character.status} - {character.species}
+                  </div>
+                  <div>Last Known Location: {character.location.name}</div>
+                </div>
+              </div>
+            </Card>
+          ))}
       </div>
     </main>
   );
---
+++ components/Filter.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import React, { ChangeEvent } from "react";
+
+type Props = {
+  locations: string[];
+  searchParams: { status: string; location: string };
+};
+
+const Filter = ({ locations, searchParams }: Props) => {
+  const router = useRouter();
+  const { status, location } = searchParams;
+
+  // Handles status change
+  const handleStatus = (e: ChangeEvent<HTMLSelectElement>) => {
+    const selectedStatus = e.target.value;
+    const queryParams = new URLSearchParams(window.location.search);
+
+    if (selectedStatus === "All") {
+      queryParams.delete("status");
+    } else {
+      queryParams.set("status", selectedStatus);
+    }
+
+    if (location) {
+      queryParams.set("location", location);
+    }
+
+    router.replace(`/?${queryParams.toString()}`);
+  };
+
+  // Handles location change
+  const handleLocation = (e: ChangeEvent<HTMLSelectElement>) => {
+    const selectedLocation = e.target.value;
+    const queryParams = new URLSearchParams(window.location.search);
+
+    queryParams.set("location", selectedLocation);
+
+    if (status && status !== "All") {
+      queryParams.set("status", status);
+    }
+
+    router.replace(`/?${queryParams.toString()}`);
+  };
+
+  return (
+    <div>
+      <form className="flex gap-4">
+        <div className="flex gap-2">
+          <label className="text-white" htmlFor="location">
+            Location
+          </label>
+          <select
+            name="location"
+            id="location"
+            value={location}
+            onChange={handleLocation}
+            className="p-2 bg-gray-800 text-white"
+          >
+            {locations.map((loc: string, i: number) => (
+              <option key={loc + i} value={loc}>
+                {loc}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="flex gap-2">
+          <label className="text-white" htmlFor="status">
+            Status
+          </label>
+          <select
+            name="status"
+            id="status"
+            value={status}
+            onChange={handleStatus}
+            className="p-2 bg-gray-800 text-white"
+          >
+            <option value="All">All</option>
+            <option value="Alive">Alive</option>
+            <option value="Dead">Dead</option>
+            <option value="unknown">Unknown</option>
+          </select>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Filter;
